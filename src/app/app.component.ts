069f39bed4b49a2ef1c81f688cdd0f6123862dd0
sanyapd <sanyapdias@gmail.com>
2022-03-08 22:57:11 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = '0703Angular';
+    numero : number = 0;
+    incrementar(): void {
+      this.numero = this.numero + 1
+    }
+    decrementar(): void{
+      if(this.numero > 0){
+      this.numero = this.numero - 1
+      }
+    }
+}
